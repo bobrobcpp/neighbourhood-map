eceaecb7337dc9da1ca0ab0383e41485e38a316f
bobrobcpp <king.roob@gmail.com>
2017-08-20 15:25:24 +0100

    Simple bootstrap setup

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 
-
-var geocoder;
 var map;
 var marker;
-var directionsService;
-var locArray = [];
+var locations = [
+   {lat: 51.507351, lng: -0.127758},
+   {lat: 51.500175, lng: -0.133233},
+   {lat: 51.500729, lng: -0.124625 },
+   {lat: 51.513845, lng: -0.098351},
+   {lat: 51.519413, lng: -0.126957},
+   {lat: 51.513614, lng: -0.136549},
+   {lat: 51.511894, lng: -0.159366},
+   {lat: 51.5299092, lng: -0.1860307}
+   ]
 
 
 function initMap() {
 
-     directionsService = new google.maps.DirectionsService();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    geocoder = new google.maps.Geocoder();
-    directionsDisplay = new google.maps.DirectionsRenderer();
     map = new google.maps.Map(document.getElementById('map'), {
-        center: latlng,
-        zoom: 5,
+        center: locations[0],
+        zoom: 11,
     });
 
 };
