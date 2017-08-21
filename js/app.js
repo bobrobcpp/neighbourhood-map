470eb6175d27e9edf35947cc8fd2d2cb8e949661
bobrobcpp <king.roob@gmail.com>
2017-08-21 11:44:15 +0100

    View model skeleton created

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+//Google maps code
 
-var map;
-var marker;
-
+  var map;
+  var marker;
   function toggleBounce() {
     if (this.getAnimation() !== null) {
       this.setAnimation(null);
@@ -10,29 +10,27 @@ var marker;
     }
   }
 
-function initMap() {
-
-
+  function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: model.locations[0],
         zoom: 13,
     });
-    model.locations.forEach(function(loc, index, array){
-      mark = new google.maps.Marker({
-        position: model.locations[index],
-        map: map,
-        draggable: true,
-        title: "Hello London",
-        label: "L",
-        animation: null
+      model.locations.forEach(function(loc, index, array){
+        mark = new google.maps.Marker({
+          position: model.locations[index],
+          map: map,
+          draggable: true,
+          title: "Hello London",
+          label: "L",
+          animation: null
+        });
+        mark.addListener('click', toggleBounce);
+
+
       });
-      mark.addListener('click', toggleBounce);
 
 
-    });
-
-
-};
+  };
 
 
 var model = {
@@ -50,8 +48,10 @@ var model = {
 
 
 
-var myViewModel = {
+function myViewModel(){
+
+
+
 
 }
-
 ko.applyBindings(myViewModel);
