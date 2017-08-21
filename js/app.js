669606cb58f52c9f90e0ddc2c813a941f1227159
bobrobcpp <king.roob@gmail.com>
2017-08-21 15:10:40 +0100

    Some knockout.js functionality

--- js/app.js
+++ js/app.js
@@ -32,10 +32,16 @@
 
   };
 
+function getLocationData(index){
+var baseUrl = "https://api.foursquare.com/v2/venues/search?ll=<latlng>&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
+var chosenLoc = model.locations[index];
+var newUrl = baseUrl.replace(/<latlng>/, chosenLoc.lat + "," + chosenLoc.lng);
+  console.log(newUrl)
+}
 
 var model = {
   locations: [
-   {lat: 51.507351, lng: -0.127758, place: "Random"},
+   {lat: 51.507351, lng: -0.127758},
    {lat: 51.500175, lng: -0.133233},
    {lat: 51.500729, lng: -0.124625 },
    {lat: 51.513845, lng: -0.098351},
@@ -48,10 +54,16 @@ var model = {
 
 
 
-function myViewModel(){
-
-
-
-
+function viewModel(){
+  var self = this;
+  self.locationList = ko.observableArray([]);
+  model.locations.forEach(function(loc){
+    self.locationList.push(loc);
+  });
 }
-ko.applyBindings(myViewModel);
+
+// var placeListItem = function(data){
+//   var self = this;
+//   this.lat = ko.observable(data.lat);
+// }
+ko.applyBindings(viewModel);
