1f90d3f32e5eb91c85b77fa319ab94d8e8e4b28f
bobrobcpp <king.roob@gmail.com>
2017-08-23 17:12:16 +0100

    Add broken updating data model

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@
           map: map,
           draggable: true,
           title: "Hello London",
-          label: "L",
+          label: model.locations[index].name,
           animation: null
         });
         mark.addListener('click', toggleBounce);
@@ -31,32 +31,59 @@
 
 
   };
+// end of google maps code
 
 
+//Data model
 var model = {
   locations: [
-   {lat: 51.507351, lng: -0.127758},
-   {lat: 51.500175, lng: -0.133233},
-   {lat: 51.500729, lng: -0.124625 },
-   {lat: 51.513845, lng: -0.098351},
-   {lat: 51.519413, lng: -0.126957},
-   {lat: 51.513614, lng: -0.136549},
-   {lat: 51.511894, lng: -0.159366},
-   {lat: 51.5299092, lng: -0.1860307}
+   {lat: 51.507351, lng: -0.127758, name: "1"},
+   {lat: 51.500175, lng: -0.133233, name: "2"},
+   {lat: 51.500729, lng: -0.124625, name: "3"},
+   {lat: 51.513845, lng: -0.098351, name: "4"},
+   {lat: 51.519413, lng: -0.126957, name: "5"},
+   {lat: 51.513614, lng: -0.136549, name: "6"},
+   {lat: 51.511894, lng: -0.159366, name: "7"},
+   {lat: 51.5299092, lng: -0.1860307, name: "8"}
    ]
 }
 
 
-
+// viewModel
 var viewModel =function (){
   var self = this;
-
   self.locationList = ko.observableArray( [] );
 
+  self.bigUrl = "https://api.foursquare.com/v2/search/recommendations?locale=en&explicit-lang=false&v=20170821&m=foursquare&query=Historic+Site&mode=url&radius=1000&ll=51.507351,-0.127758&limit=10&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
+  $.getJSON(self.bigUrl, function(data){
+    console.log(self.bigUrl);
+    // data.response.group.results.forEach(function(loc){
+    //   console.log(loc.venue.name + ", Latitude: " + loc.venue.location.lat + ", Longitude: " + loc.venue.location.lng);
+    //   self.locationList.push(new placeListItem(loc.venue));
+    // });
 
-  model.locations.forEach(function(loc){
-    self.locationList.push(new placeListItem(loc));
+    // for(var i = 0 ; i < data.response.group.results.length; i ++){
+    //   console.log(data.response.group.results[i].venue.name + ", Latitude: " + data.response.group.results[i].venue.location.lat + ", Longitude: " + data.response.group.results[i].venue.location.lng);
+    //   self.locationList.push(new placeListItem(data.response.group.results[i].venue));
+    // };
+
+  })
+  .then(function(data){
+      data.response.group.results.forEach(function(loc){
+      console.log(loc.venue.name + ", Latitude: " + loc.venue.location.lat + ", Longitude: " + loc.venue.location.lng);
+      self.locationList.push(new placeListItem(loc.venue));
+      });
+  })
+  .fail(function() {
+    console.log( "Error in bigUrl request, please check URL" );
   });
+
+
+
+
+  // model.locations.forEach(function(loc){
+  //   self.locationList.push(new placeListItem(loc));
+  // });
 
   self.currentItem = ko.observable(self.locationList()[0]);
 
@@ -64,23 +91,29 @@ var viewModel =function (){
 
 
 var placeListItem = function(data){
-  this.lat = ko.observable(data.lat);
-  this.name = "test";
+  // this.lat = ko.observable(data.location.lat);
+  // this.lng = ko.observable(data.location.lng);
+  this.name = ko.observable(data.name);
   this.newUrl;
+
   this.getLocationData = function(){
     var getLocationData;
-    var baseUrl = "https://api.foursquare.com/v2/venues/search?ll=<latlng>&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
+    var baseUrl = "https://api.foursquare.com/v2/venues/explore?ll=<latlng>&limit=4&section=sights&venuePhotos=1&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
     var chosenLoc = data;
-    newUrl = baseUrl.replace(/<latlng>/, chosenLoc.lat + "," + chosenLoc.lng);
+    newUrl = baseUrl.replace(/<latlng>/, chosenLoc.location.lat + "," + chosenLoc.location.lng);
+    console.log("New url = " + newUrl);
 
-    $.getJSON(newUrl, function(){
-      console.log("Success " + newUrl);
+    $.getJSON(newUrl, function(data){
+      console.log(newUrl);
+      // console.log("Success " + data.response.venues[0].name);
+      console.log("Success " + data.response.groups[0].items[0].venue.name);
+
     })
     .fail(function() {
       console.log( "Error in getJSON request, please check URL" );
     });
 
-  };
+  }
 }
 
 ko.applyBindings(new viewModel());
