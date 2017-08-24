e26db5d3a5f5be829355edb8b379ab50c701e123
bobrobcpp <king.roob@gmail.com>
2017-08-24 18:54:23 +0100

    Buggy working with self.locationTest

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@
 // end of google maps code
 
 
-//Data model
+//Data model shouldn't be used
 var model = {
   locations: [
    {lat: 51.507351, lng: -0.127758, name: "1"},
@@ -49,52 +49,56 @@ var model = {
 }
 
 
+//Actual data model
+var dataModel = {
+  locations: []
+}
+
+
 // viewModel
 var viewModel =function (){
   var self = this;
   self.locationList = ko.observableArray( [] );
-
+  self.locationTest = ko.observableArray( [] );
   self.bigUrl = "https://api.foursquare.com/v2/search/recommendations?locale=en&explicit-lang=false&v=20170821&m=foursquare&query=Historic+Site&mode=url&radius=1000&ll=51.507351,-0.127758&limit=10&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
   $.getJSON(self.bigUrl, function(data){
     console.log(self.bigUrl);
-    // data.response.group.results.forEach(function(loc){
-    //   console.log(loc.venue.name + ", Latitude: " + loc.venue.location.lat + ", Longitude: " + loc.venue.location.lng);
-    //   self.locationList.push(new placeListItem(loc.venue));
-    // });
-
-    // for(var i = 0 ; i < data.response.group.results.length; i ++){
-    //   console.log(data.response.group.results[i].venue.name + ", Latitude: " + data.response.group.results[i].venue.location.lat + ", Longitude: " + data.response.group.results[i].venue.location.lng);
-    //   self.locationList.push(new placeListItem(data.response.group.results[i].venue));
-    // };
-
-  })
-  .then(function(data){
-      data.response.group.results.forEach(function(loc){
+    data.response.group.results.forEach(function(loc){
       console.log(loc.venue.name + ", Latitude: " + loc.venue.location.lat + ", Longitude: " + loc.venue.location.lng);
-      self.locationList.push(new placeListItem(loc.venue));
-      });
+// self.locationList.push(new placeListItem(loc.venue));
+      this. test = new placeListItem(loc.venue);
+      dataModel.locations.push({name: this.test.name});
+      // dataModel.locations.push(new placeListItem(loc.venue));
+    });
+    dataModel.locations.forEach(function(boc){
+      self.locationList.push(new placeListItem(boc));
+    });
   })
   .fail(function() {
     console.log( "Error in bigUrl request, please check URL" );
   });
 
+//Shouldn't be required
+      model.locations.forEach(function(coc){
+        self.locationTest.push(new placeListItem(coc));
+      });
 
-
-
-  // model.locations.forEach(function(loc){
-  //   self.locationList.push(new placeListItem(loc));
-  // });
-
-  self.currentItem = ko.observable(self.locationList()[0]);
+    // self.currentItem = ko.observable(self.locationList()[0]);
+// Should be bound to locationList above!
+  self.currentItem = ko.observable(self.locationTest()[0]);
 
   }
 
 
 var placeListItem = function(data){
+  this.lat = ko.observable(data.lat);
+  this.lat = ko.observable(data.lng);
+
   // this.lat = ko.observable(data.location.lat);
   // this.lng = ko.observable(data.location.lng);
   this.name = ko.observable(data.name);
   this.newUrl;
+
 
   this.getLocationData = function(){
     var getLocationData;
