f5b5583ca85cc2d3a07049b7385ed5370604d368
bobrobcpp <king.roob@gmail.com>
2017-08-30 13:21:47 +0100

    Passing self.locationList into placeMarker().  Start refactoring toward using a markers observableArray

--- js/app.js
+++ js/app.js
@@ -10,19 +10,20 @@ var dataModel = {
 var viewModel =function (){
   var self = this;
   self.locationList = ko.observableArray( [] );
-  self.locationTest = ko.observableArray( [] );
+  // self.markers = ko.observableArray( [] );
   self.bigUrl = "https://api.foursquare.com/v2/search/recommendations?locale=en&explicit-lang=false&v=20170821&m=foursquare&query=Historic+Site&mode=url&radius=1000&ll=51.507351,-0.127758&limit=10&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
   $.getJSON(self.bigUrl, function(data){
     console.log(self.bigUrl);
     data.response.group.results.forEach(function(loc){
       console.log(loc.venue.name + ", Latitude: " + loc.venue.location.lat + ", Longitude: " + loc.venue.location.lng);
       this.test = new placeListItem(loc.venue);
+      // self.markers.push(this.test.mark);
       dataModel.locations.push({lat: this.test.lat, lng: this.test.lng, name: this.test.name, getLocationData: this.test.getLocationData, rating: this.test.rating, checkins: this.test.checkins, formattedAddress: [this.test.formattedAddress], mark: this.test.mark});
     });
     dataModel.locations.forEach(function(boc){
       self.locationList.push(boc);
     });
-    placeMarker();
+            placeMarker(self.locationList());
 
   })
   .then(function() {
@@ -38,6 +39,7 @@ var viewModel =function (){
 
 
 var placeListItem = function(data){
+  var that = this;
   this.lat = ko.observable(data.location.lat);
   this.lng = ko.observable(data.location.lng);
   this.name = ko.observable(data.name);
@@ -51,12 +53,24 @@ var placeListItem = function(data){
           map: map,
           draggable: true,
           // label: (index+1).toString(),
-          animation: null
+          animation: null,
+          getLoc: function(){that.getLocationData();},
+          toggleBounce: function() {
+            var that = this;
+            if (this.getAnimation() !== null) {
+              this.setAnimation(null);
+            } else {
+
+              this.setAnimation(google.maps.Animation.BOUNCE);
+              setTimeout(function(){
+                that.setAnimation(null);
+               },2000);
+            }
+          }
         });
 
 
   this.getLocationData = function(){
-    var getLocationData;
     var baseUrl = "https://api.foursquare.com/v2/venues/explore?ll=<latlng>&radius=10&limit=4&section=sights&venuePhotos=1&client_id=NYRK42K0WXYTE2W5YZQYISSRSPI13N40ZNX0VLOOLHDALLM0&client_secret=UJLKHHZUJOIFPXZZYJTQ3UUXCEI1OV4JOSQ1OOTWKIZHWWRX&v=20170821";
     var chosenLoc = data;
     newUrl = baseUrl.replace(/<latlng>/, chosenLoc.location.lat + "," + chosenLoc.location.lng);
@@ -78,23 +92,9 @@ ko.applyBindings(new viewModel());
 
 
 
-
 //Google maps code
 
   var map;
-  function toggleBounce() {
-    var that = this;
-    if (this.getAnimation() !== null) {
-      this.setAnimation(null);
-    } else {
-
-this.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){
-              that.setAnimation(null);
-      },2000);
-    }
-  }
-
   function initMap() {
     var startCoord = {lat:51.507351, lng: -0.127758};
     map = new google.maps.Map(document.getElementById('map'), {
@@ -105,16 +105,19 @@ this.setAnimation(google.maps.Animation.BOUNCE);
   }
 
 
-  function placeMarker() {
+
+
+  function placeMarker(locations) {
       var infowindow = new google.maps.InfoWindow();
-    var latlngbounds = new google.maps.LatLngBounds();
-      dataModel.locations.forEach(function(loc, index, array){
+      var latlngbounds = new google.maps.LatLngBounds();
+      locations.forEach(function(loc, index, array){
         var myLatLng = new google.maps.LatLng(loc.lat(), loc.lng());
         latlngbounds.extend(myLatLng);
 
         var mark = loc.mark;
-        mark.addListener('click', toggleBounce);
         mark.addListener('click', function(){
+          this.getLoc();
+          this.toggleBounce();
         var contentString = "<h4>" + (loc.name()).toString() + "</h4><br><h5> Address: </h5><div id='info-window'>";
         for (i = 0; i < loc.formattedAddress[0]()[0].length; i ++){
           contentString += ("<p>" + loc.formattedAddress[0]()[0][i] + "<br> </p>");
